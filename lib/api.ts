506e2c0dff0e535db032b94828a1c19dd1095454
navyadurve <navyadurve@gmail.com>
2026-04-02 09:42:38 +0530

    base url change

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://repo-backend-wcq2.onrender.com'
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://dbms-project-u67u.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
